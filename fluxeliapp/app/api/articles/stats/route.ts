499ee1b4f3a64a7d9af3d0c825ef242505da66be
Ophiuchus12 <cgoisque@yahoo.fr>
2025-08-31 15:00:50 +0200

    changes

--- fluxeliapp/app/api/articles/stats/route.ts
+++ fluxeliapp/app/api/articles/stats/route.ts
@@ -4,22 +4,22 @@ import { open } from 'sqlite'
 import path from 'path'
 
 async function openDb() {
-    const dbPath = path.resolve(process.cwd(), '../rss_feed.db')
-    return open({
-        filename: dbPath,
-        driver: sqlite3.Database,
-    })
+  const dbPath = path.resolve(process.cwd(), '../rss_feed.db')
+  return open({
+    filename: dbPath,
+    driver: sqlite3.Database,
+  })
 }
 
 export async function GET(req: Request) {
-    const db = await openDb();
+  const db = await openDb();
 
-    const stats = await db.get(`
+  const stats = await db.get(`
     SELECT 
       COUNT(*) AS countArticles,
       COUNT(DISTINCT category) AS countCategories
     FROM articles
   `);
 
-    return NextResponse.json(stats);
+  return NextResponse.json(stats);
 }
---
+++ fluxeliapp/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://fluxelia.fr',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+}
